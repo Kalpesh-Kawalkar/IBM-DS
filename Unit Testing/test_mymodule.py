ccaf16a8ed26243cbe7f0b7fa053c0cb6f0f5723
Kalpesh Kawalkar <102687995+Kalpesh-Kawalkar@users.noreply.github.com>
2023-10-17 19:59:45 +0530

    Update test_mymodule.py

--- Unit Testing/test_mymodule.py
+++ Unit Testing/test_mymodule.py
@@ -1,6 +1,6 @@
 import unittest
 
-from mymodule import square, double
+from mymodule import square, double, add
 
 class TestSquare(unittest.TestCase): 
     def test1(self): 
@@ -14,5 +14,14 @@ class TestDouble(unittest.TestCase):
         self.assertEqual(double(2), 4) # test when 2 is given as input the output is 4.
         self.assertEqual(double(-3.1), -6.2) # test when -3.1 is given as input the output is -6.2.
         self.assertEqual(double(0), 0) # test when 0 is given as input the output is 0.
+
+class TestAdd(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(add(2, 4), 6)
+        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(2.3, 3.6), 5.9)
+        self.assertEqual(add('hello', 'world'), 'helloworld')
+        self.assertEqual(add(2.3000, 4.300), 6.6)
+        self.assertNotEqual(add(-2, -2), 0)
         
 unittest.main()
